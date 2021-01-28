341b1f300bf03c02a0512e0904a57ca7fa584c17
Anko-Jipsa <Sjfksrjs1!>
2021-01-28 21:44:01 +0000

    EX2 ST436

---
+++ ST436/EX2/EX2.R
@@ -0,0 +1,42 @@
+library(e1071)
+library(pracma)
+df = read.csv("~/Downloads/^KS11.csv")
+
+# Data pre-processing
+df = df %>% replace(.=="null", NA)
+df = na.omit(df)
+df$Date = as.Date(df$Date) 
+df$Adj.Close = as.numeric(df$Adj.Close)
+
+# Log-return
+df$log_ret = c(NA, diff(log(df$Adj.Close), lag=1))
+df = na.omit(df)
+
+## Stylised facts:
+# (a) The returns oscillate around zero
+plot(x=df$Date, y=df$log_ret, 
+     type="l", main="Log Return", ylab = "Value")
+mean(df$log_ret) # Almost 0.
+
+# (b) The returns are heavy-tailed.
+xseq = linspace(min(df$log_ret), max(df$log_ret), n = 100)
+x<-seq(min(df$log_ret), max(df$log_ret),by=0.02)
+hist(df$log_ret, prob=TRUE, freq=F,density=50, breaks=50)
+lines(xseq, dnorm(xseq, mean(df$log_ret), sd(df$log_ret)), col="blue")
+kurtosis(df$log_ret) #10.81811, heavy tail
+
+# (c) The returns display a downward skew.
+skewness(df$log_ret) # Negative skewness
+
+# (d) There is a leverage effect in the returns.
+y=abs(df$log_ret[2:length(df$log_ret)])
+x=df$log_ret[1:length(df$log_ret)-1]
+s = ksmooth(x, y)
+plot(x,y)
+lines(s, lwd = 2, col = 2)
+
+# (e) The sample autocorrelation of the returns is close to zero for almost all lags.
+acf(df$log_ret)
+
+# (f) The sample autocorrelation of the squared returns is large and positive for many lags
+acf((df$log_ret)^2)
